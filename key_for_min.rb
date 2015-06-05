ca0775c9e303c56e052ccbc3b10c48e36dd3c3ff
apradhn <abhijit.pradhan6@gmail.com>
2015-06-05 13:54:01 -0400

    all done

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,23 @@
 
 def key_for_min_value(name_hash)
   # code goes here
+  if name_hash.empty?
+    nil
+  else
+    nums = []
+    name_hash.each do |key, value| 
+      if nums.empty? 
+        nums << value
+      else
+        if value > nums[0]
+          nums.insert(1, value)
+        elsif value < nums[0]
+          nums.unshift(value)
+        end
+      end
+    end
+    min_value = nums[0]
+    name_hash.find {|key, value| value == min_value}[0]    
+  end
+
 end
